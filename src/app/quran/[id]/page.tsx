c868a7319878ec38ea57a6d04b0a79841ad14ae6
DannielLima <karlosdanniellima@gmail.com>
2024-12-19 22:43:53 -0300

    test design quran page

--- src/app/quran/[id]/page.tsx
+++ src/app/quran/[id]/page.tsx
@@ -62,26 +62,26 @@ export default function QuranSurahPage({ params }: Params) {
   const { surahData, translationData } = use(getSurahAndTranslation(id));
 
   return (
-    <div className="min-h-screen text-dark-gray p-6">
-      <div className="max-w-4xl mx-auto p-8 bg-light-beige shadow-lg rounded-lg border-4 border-dark-green">
+    <div className="min-h-screen text-dark-gray p-4">
+      <div className="max-w-3xl mx-auto p-6 bg-light-beige">
         <Image
           src="/img/bismillah-img.png"
-          alt="bismillah"
-          width={500}
-          height={500}
-          className="rounded-lg max-w-full mx-auto"
+          alt="Bismillah"
+          width={400}
+          height={400}
+          className="mx-auto mb-8 pointer-events-none"
         />
-        <h1 className="text-4xl font-serif text-center mb-6 text-dark-gray md:text-5xl">
+        <h1 className="text-3xl md:text-4xl font-serif text-center text-dark-gray mb-6">
           {surahData.name}
         </h1>
-        <hr className="border-t border-dark-green my-4" />
-        <ul className="space-y-8">
+        <hr className="border-t border-y-dark-gray my-6" />
+        <ul className="space-y-10">
           {Object.entries(surahData.verse).map(([key, verse]) => (
-            <li key={key} className="text-center">
-              <p className="text-3xl font-semibold font-arabic text-dark-gray leading-loose mb-3">
+            <li key={key} className="text-right">
+              <p className="text-xl md:text-2xl font-semibold font-arabic text-[#1a1a1a] leading-relaxed mb-4">
                 {verse}
               </p>
-              <p className="text-lg italic font-lato text-dark-gray">
+              <p className="text-base md:text-lg italic font-lato text-[#4f4f4f]">
                 {translationData.verse[key]}
               </p>
             </li>
